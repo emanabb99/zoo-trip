19dfe43772771fbfeb4dd820929828a2e6de59e2
Eman Abbas <eman.abbas@autotrader.co.uk>
2026-03-31 15:17:00 +0100

    Bus seating plan - Eman

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,37 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 public class Main {
+
+    static void main() {
+        ArrayList<String> busSeatingPlan = new ArrayList<>();
+
+        busSeatingPlan.add("Ryan");
+        busSeatingPlan.add("Brandon");
+        busSeatingPlan.add("Rebecca");
+        busSeatingPlan.add("Richard");
+        busSeatingPlan.add("Jessica");
+        busSeatingPlan.add("Brian");
+        busSeatingPlan.add("Antoinne");
+        busSeatingPlan.add("Grace");
+        busSeatingPlan.add("Zee");
+        busSeatingPlan.add("Ibrahim");
+
+        System.out.println(busSeatingPlan);
+
+        Collections.swap(busSeatingPlan,busSeatingPlan.indexOf("Ibrahim"),0);
+
+        Collections.swap(busSeatingPlan,busSeatingPlan.indexOf("Rebecca"),9);
+
+        Collections.swap(busSeatingPlan,busSeatingPlan.indexOf("Richard"),busSeatingPlan.indexOf("Antoinne")-1);
+
+        Collections.swap(busSeatingPlan,busSeatingPlan.indexOf("Ryan"),busSeatingPlan.indexOf("Rebecca")-1);
+
+        System.out.println(busSeatingPlan);
+
+
+
+
+    }
 }
